51649a8901f442c60a1aaabddd680256713c1fec
4ndre <113845523+st4rgate@users.noreply.github.com>
2023-06-26 19:15:19 +0200

    Aggiunta app sezione 14 - AspMediaLinksApp

---
+++ S14. Configuration/AspMediaLinksApp/AspMediaLinksApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using AspMediaLinksApp.Option_Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AspMediaLinksApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _options;
+
+        public HomeController(IOptions<SocialMediaLinksOptions> options)
+        {
+            _options = options.Value;
+        }
+        [method: HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Facebook = _options.Facebook;
+            ViewBag.Instagram = _options.Instagram;
+            ViewBag.Twitter = _options.Twitter;
+            ViewBag.Youtube = _options.Youtube;
+
+            return View();
+        }
+    }
+}
---
+++ S14. Configuration/AspMediaLinksApp/AspMediaLinksApp/Program.cs
@@ -0,0 +1,14 @@
+using AspMediaLinksApp.Option_Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<SocialMediaLinksOptions>(builder.Configuration.GetSection("SocialMediaLinks"));
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
